3aaf7bbc433bf9ab65d2dd7d0c955eedc5c1442a
Ubuntu <nskelsey@gmail.com>
2014-08-31 22:54:11 +0000

    Functional base64 to raw data + json mapping

---
+++ assert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+http POST localhost:1055/upload Content-Type:text/plain < hello.txt
+
--- main.go
+++ main.go
@@ -4,85 +4,172 @@ import (
 	"crypto/sha256"
 	"database/sql"
 	"encoding/base64"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"math"
 	"net/http"
 	"net/url"
+	"strconv"
 	"time"
 
 	_ "code.google.com/p/go-sqlite/go1/sqlite3"
 )
 
 type Item struct {
-	Hash      string
-	Firstseen int64
-	Raw       []byte
+	Hash        string
+	FirstSeen   int64
+	ContentType string
+	Raw         []byte
 }
 
 var (
-	conn        *sql.Db
-	select_item *sql.Stmt
-)
-
-const (
-	MAX_FILE_SIZE int = 10 * math.Pow(8, 20) // 10 MB max filesize
+	conn          *sql.DB
+	selectItem    *sql.Stmt
+	storeItem     *sql.Stmt
+	MAX_FILE_SIZE int = 10 * int(math.Pow(2, 20)) // 10 MB max filesize
 )
 
 func compHash(raw []byte) string {
-	bytes := sha256.sum256(raw)
+	bytes := sha256.Sum256(raw)
 	s := base64.URLEncoding.EncodeToString(bytes[:])
 	return s
 }
 
-func makeItem(raw []byte) *Item {
-	item = &Item{
-		Hash:      compHash(raw),
-		Firstseen: time.Now().Unix(),
-		Raw:       raw,
+func makeItem(raw []byte, contType string) *Item {
+	item := &Item{
+		Hash:        compHash(raw),
+		FirstSeen:   time.Now().Unix(),
+		ContentType: contType,
+		Raw:         raw,
 	}
 	return item
 }
 
-func correctLen(target int, url url.URL) bool {
+func scanItem(rows *sql.Rows) (*Item, error) {
+	var hash, conttype string
+	var firstseen int64
+	var raw []byte = make([]byte, 0)
+
+	err := rows.Scan(&hash, &firstseen, &conttype, &raw)
+	if err != nil {
+		return nil, err
+	} else {
+		item := &Item{
+			Hash:        hash,
+			FirstSeen:   firstseen,
+			ContentType: conttype,
+			Raw:         raw,
+		}
+		return item, nil
+	}
+}
+
+func correctLen(target int, url *url.URL) bool {
 	if len(url.Path) != target {
 		return false
 	}
+	return true
 }
 
 func handleUpload(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		http.Error(w, "", 405)
+		http.Error(w, "Method not allowed.", 405)
 		return
 	}
 
-	// TODO do not just read all.
-	buf, err := ioutil.ReadAll(req.Body)
+	buf, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "bad buffer", 500)
+		http.Error(w, "Could not read file.", 500)
 		return
 	}
 
-	if len(buf) > MAX_FILE_SIZE {
-		http.Error(w, "Request entity too large", 413)
+	cont_type := r.Header.Get("Content-Type")
+	if cont_type == "" {
+		http.Error(w, "No Content-Type provided", 403)
+	}
+	item := makeItem(buf, cont_type)
+
+	rows, err := selectItem.Query(item.Hash)
+	defer rows.Close()
+	if err != nil {
+		http.Error(w, "Database error.", 500)
+	}
+	if rows.Next() {
+		http.Error(w, "Entity already stored", 409)
 		return
 	}
-	item := makeItem(buf)
 
-	res, err := select_item.Exec(item.Hash)
+	res, err := storeItem.Exec(item.Hash, item.FirstSeen, item.ContentType, item.Raw)
+	if err != nil {
+		http.Error(w, "Database error.", 500)
+		return
+	}
+	i, err := res.RowsAffected()
+	if i != 1 || err != nil {
+		http.Error(w, "Database error.", 500)
+		return
+	}
 
+	itemUrl := "http://localhost:1055/" + item.Hash
+	w.WriteHeader(201)
+	w.Write([]byte(itemUrl))
+}
+
+func getItem(w http.ResponseWriter, itemHash string) *Item {
+	rows, err := selectItem.Query(itemHash)
+	defer rows.Close()
+	if err != nil {
+		http.Error(w, "Database error.", 500)
+		return nil
+	}
+	if !rows.Next() {
+		http.Error(w, "Item not found in database.", 404)
+		return nil
+	}
+
+	item, err := scanItem(rows)
+	if err != nil {
+		http.Error(w, "Database error.", 500)
+		return nil
+	}
+	return item
 }
 
 func getJsonItem(w http.ResponseWriter, r *http.Request) {
-	if !correctLen(r.URL, 5+4) {
-		http.Error(w, "", 404)
+	if !correctLen(len("/api/")+44, r.URL) {
+		http.Error(w, "Improper url format", 500)
 	}
+	itemHash := r.URL.Path[5:]
+	item := getItem(w, itemHash)
+	if item == nil {
+		return
+	}
+
+	bytes, err := json.Marshal(item)
+	if err != nil {
+		http.Error(w, "JSON marshal failed.", 500)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(item.Raw)))
+	w.Write(bytes)
 }
 
 func getRawItem(w http.ResponseWriter, r *http.Request) {
-	if !correctLen(r.URL, 4) {
-		http.Error(w, "", 404)
+	if !correctLen(45, r.URL) {
+		http.Error(w, "Improper url format", 500)
+		return
 	}
+
+	itemHash := r.URL.Path[1:]
+	item := getItem(w, itemHash)
+	if item == nil {
+		return
+	}
+
+	w.Header().Set("Content-Type", item.ContentType)
+	w.Header().Set("Content-Length", strconv.Itoa(len(item.Raw)))
+	w.Write(item.Raw)
 }
 
 func main() {
@@ -92,7 +179,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	select_item, err = conn.Prepare("SELECT hash, date, raw FROM items where hash = $1")
+	selectItem, err = conn.Prepare("SELECT hash, firstseen, conttype, raw FROM items where hash = $1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	storeItem, err = conn.Prepare("INSERT INTO items (hash, firstseen, conttype, raw) VALUES($1, $2, $3, $4)")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -100,5 +191,5 @@ func main() {
 	http.HandleFunc("/upload", handleUpload)
 	http.HandleFunc("/api/", getJsonItem)
 	http.HandleFunc("/", getRawItem)
-	http.ListenAndServe(":1055", nil)
+	log.Fatal(http.ListenAndServe(":1055", nil))
 }
